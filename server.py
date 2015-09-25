b2f66a688ff98a48387063aa71993a9e6b3e3399
Ubuntu <ubuntu@ip-172-31-31-254.us-west-2.compute.internal>
2015-09-25 04:06:44 +0000

    init

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 CAFFE_ROOT = "/home/ubuntu/caffe/"
-# CAFFE_ROOT = '/Users/bski/Project/caffe/'
 import sys
 sys.path.insert (0, CAFFE_ROOT + "python")
 import caffe, numpy as np
@@ -24,9 +23,7 @@ app.config ['MONGO_PORT'] = MONGO_PORT
 app.config ['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 connection = Connection (app.config['MONGO_HOST'], app.config['MONGO_PORT'])
 
-# caffe.set_mode_cpu()
-caffe.set_mode_gpu()
-caffe.set_device(0)
+caffe.set_mode_cpu()
 hdd_labels_file = MODELS_ROOT + "labels.txt"
 labels = None
 num_outs = 0
@@ -44,7 +41,6 @@ c0 = caffe.Classifier (
             image_dims = (256, 256)
 )
 classifiers = [c0]
-
 
 def classify_image (image_file_name):
     image = caffe.io.load_image(image_file_name)
@@ -116,44 +112,6 @@ def hdd_classify ():
     #     resp.status_code = 500
     #     return resp
 
-
-@app.route("/classify")
-def classify():
-    try:
-        num_outs = 14
-        image_url = request.args.get ("image_url")
-        image_file_name = IMAGE_ROOT + image_url.split("/")[-1]
-        urllib.urlretrieve (image_url, image_file_name)
-        app.logger.debug ("[classifier] image loaded to " + image_file_name)
-
-        image = caffe.io.load_image(image_file_name)
-        resized_image = caffe.io.resize_image (image, (256,256,3))
-        res = np.zeros (num_outs * len (classifiers)).reshape (num_outs, len(classifiers))
-        for i, x in enumerate (classifiers):
-            res[:,i] = x.predict ([resized_image])[0]
-        avg_probs = np.average (res, axis=1)
-        top_k_idx = avg_probs.argsort()[-1:-6:-1]
-        class_res = connection.Classification()
-        class_res['image_url'] = image_url
-        class_res['date_created'] = datetime.datetime.now()
-        class_res['top_3'] = []
-        for x in top_k_idx.tolist():
-            res_dict = {}
-            res_dict["class_name"] = labels.tolist()[x][0]
-            res_dict["prob"] = avg_probs.tolist()[x]
-            app.logger.debug (str(res_dict) + " " + image_file_name)
-            class_res['top_3'].append (res_dict)
-        class_res['top_1'] = class_res['top_3'][0]
-        class_res.save()
-        app.logger.debug ("[classifier] classification result saved." + str(class_res.to_json()))
-        response = Response (response = class_res.to_json(), status=200, mimetype="application/json")
-        return response
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        app.logger.error (" ".join(traceback.format_tb (exc_traceback)))
-        resp = jsonify ({"msg": "Server Error"})
-        resp.status_code = 500
-        return resp
 
 if __name__ == "__main__":
     if not app.debug:
